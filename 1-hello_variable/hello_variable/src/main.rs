6d4fb362a83164c1fabc7185e2de80484043e33e
Artify <richard.tichy@mensa.cz>
2022-06-15 23:58:07 +0200

    Added 1st example `hello_variable`

---
+++ 1-hello_variable/hello_variable/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = 5;      // type from init value
+    let b: i32 = 15;    // explicit type
+    let c = 0x15;   // value in hexadecimal
+
+    println!("A is {}.", a);
+    println!("B is {}.", b);
+    println!("C is {}.", c);
+}
